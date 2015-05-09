326bb8d9142ebc0e1dfc5815df1c65a7b87439bc
Zoltan <zdebre@gmail.com>
2015-05-09 22:55:37 +1200

    Add main class

--- src/main/java/org/hibernate/tutorial/domain/Event.java
+++ src/main/java/org/hibernate/tutorial/domain/Event.java
@@ -4,29 +4,29 @@ import java.util.Date;
 
 public class Event {
     private Long   id;
-    
+
     private String title;
     private Date   date;
-    
+
     public Event() {
     }
-    
+
     public Date getDate() {
         return date;
     }
-    
+
     public Long getId() {
         return id;
     }
-    
+
     public String getTitle() {
         return title;
     }
-    
+
     public void setDate(Date date) {
         this.date = date;
     }
-    
+
     public void setTitle(String title) {
         this.title = title;
     }
